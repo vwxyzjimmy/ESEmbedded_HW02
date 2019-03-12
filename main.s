e0ab4390fe50513049b76239612c0f8c4a282305
vwxyzjimmy <vwxyzjimmy@gmail.com>
2019-03-12 19:39:44 +0800

    add HW02

--- main.s
+++ main.s
@@ -11,6 +11,26 @@ _start:
 	//
 	//branch w/o link
 	//
+	movs	r1, #1
+	movs	r2, #2
+	movs	r3, #3
+
+	push	{r1, r2, r3}
+	pop		{r4, r5, r6}
+
+	movs	r4, #0
+	movs	r5, #0
+	movs	r6, #0
+
+	push	{r3, r2, r1}
+	pop		{r4, r5, r6}
+
+	movs	r4, #0
+	movs	r5, #0
+	movs	r6, #0
+
+	push	{r1, r2, r3}
+	pop		{r6, r5, r4}
 	b	label01
 
 label01:
